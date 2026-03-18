d6fa08dd3d20b1fe9152b8d32c638e5550be4463
Diego <diportobr@gmail.com>
2026-03-18 09:58:23 -0300

    Config: Force PWA to update and take control immediately

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        skipWaiting: true,
+        clientsClaim: true,
+      },
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'DentalFlow',
