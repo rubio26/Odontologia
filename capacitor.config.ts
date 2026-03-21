4768819675c778a8523782eb8388013ae121b865
Diego <diportobr@gmail.com>
2026-03-21 13:05:09 -0300

    Play Store Preparation: Configure Capacitor for Android, add permissions and convenience scripts.

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lumini.studio.app',
+  appName: 'Lumini Studio',
+  webDir: 'dist'
+};
+
+export default config;
